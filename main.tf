ace4f59ec64944e58b202026d917efc65b68b4ea
jack2784 <49051469+jack2784@users.noreply.github.com>
2022-03-17 15:55:50 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
 provider "google" {
   project = "terraform-344402"
-  region  = "europe-west2"
-  zone    = "europe-west2-b"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
+  tags         = ["web", "dev"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "cos-cloud/cos-stable"
     }
   }
 
   network_interface {
-    network = "default"
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
   }
 }
-
-
